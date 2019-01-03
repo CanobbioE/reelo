198e52979d531ad28793d95ada02a201be43b710
AnnaOpss <hi@anna.so>
2019-01-03 02:15:41 -0800

    Renamed variables containing 'id' to 'ID'

--- tools/input.go
+++ tools/input.go
@@ -14,18 +14,18 @@ import (
 const RANK_PATH = "./ranks"
 
 // parseRankingFile reads a ranking from the correct file using the specified
-// format. The file's name must be "year_category.txt"
+// format. The file's name must be in the format of "year_category.txt"
 func parseRankingFile(year int, category, format string) {
 	filePath := fmt.Sprintf("%s/%d/%d_%s.txt", RANK_PATH, year, year, category)
 	file, err := os.Open(filePath)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal("Couldn't open file.", err)
 	}
 	defer file.Close()
 
 	// add the current year+category to the db
 	db := rdb.DB()
-	gId := rdb.Add(db, "giochi", year, category)
+	gID := rdb.Add(db, "giochi", year, category)
 	db.Close()
 
 	f := newFormat(strings.Split(strings.ToLower(format), " "))
@@ -34,7 +34,7 @@ func parseRankingFile(year int, category, format string) {
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		data := strings.Split(scanner.Text(), " ")
-		updateDb(data, f, gId)
+		updateDb(data, f, gID)
 		if err := scanner.Err(); err != nil {
 			log.Fatal(err)
 		}
@@ -43,19 +43,19 @@ func parseRankingFile(year int, category, format string) {
 
 // updateDb updates the database by adding all the data read from a single row
 // of the Ranking's file.
-func updateDb(data []string, f *Format, gId int) {
-	var pId, time int
+func updateDb(data []string, f *Format, gID int) {
+	var pID, time int
 	db := rdb.DB()
 	defer db.Close()
 	// Add the player information only if he doesn't already exist in the db
 	if !rdb.ContainsPlayer(db, data[f.Name], data[f.Surname]) {
-		pId = rdb.Add(db, "giocatore", data[f.Name], data[f.Surname])
+		pID = rdb.Add(db, "giocatore", data[f.Name], data[f.Surname])
 	} else {
 		q := `
 		SELECT id FROM Giocatore
 		WHERE nome = ? AND cognome = ?
 		`
-		err := db.QueryRow(q, data[f.Name], data[f.Surname]).Scan(&pId)
+		err := db.QueryRow(q, data[f.Name], data[f.Surname]).Scan(&pID)
 		if err != nil {
 			log.Fatal(err)
 		}
@@ -68,6 +68,6 @@ func updateDb(data []string, f *Format, gId int) {
 			time = 0
 		}
 	}
-	rId := rdb.Add(db, "risultato", time, data[f.Exercises], data[f.Score])
-	rdb.Add(db, "partecipazione", pId, gId, rId)
+	rID := rdb.Add(db, "risultato", time, data[f.Exercises], data[f.Score])
+	rdb.Add(db, "partecipazione", pID, gID, rID)
 }
