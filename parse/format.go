7c3f5b2ba9835704df7901f7cebd4d3e08e86a0f
AnnaOpss <hi@anna.so>
2019-01-06 10:44:37 -0800

    Started refactoring the parse package

--- parse/format.go
+++ parse/format.go
@@ -1,32 +1,36 @@
-package parse
+package main
+
+import (
+	"log"
+	"strings"
+)
+
+var possibleFormats = map[string]interface{}{
+	"cognome":          struct{}{},
+	"nome":             struct{}{},
+	"esercizi":         struct{}{},
+	"punti":            struct{}{},
+	"tempo":            struct{}{},
+	"città":            struct{}{},
+	"città(provincia)": struct{}{},
+}
 
 // Format represents the format used in a ranking file.
-// Each field represents an information's index inside a line of the file.
-type Format struct {
-	Exercises, Score, Time int
-	Name, Surname          int
-	City                   int
-}
+// Each element of the map links to the index of the column's number.
+type Format map[string]int
 
 // newFormat returns a new format based on the slice of string passed
-func newFormat(ff []string) *Format {
-	return &Format{
-		Name:      indexOf(ff, "nome"),
-		Surname:   indexOf(ff, "cognome"),
-		City:      indexOf(ff, "sede"),
-		Exercises: indexOf(ff, "esercizi"),
-		Score:     indexOf(ff, "punteggio"),
-		Time:      indexOf(ff, "tempo"),
-	}
-}
+func newFormat(input []string) Format {
+	format := make(map[string]int)
 
-// indexOf returns the position of the pattern's first occurency inside
-// the ss slice
-func indexOf(ss []string, pattern string) int {
-	for i, s := range ss {
-		if s == pattern {
-			return i
+	for index, value := range input {
+		value = strings.ToLower(value)
+		if _, ok := possibleFormats[value]; !ok {
+			log.Fatalf("format value %s not recognized.", value)
 		}
+
+		format[value] = index
 	}
-	return -1
+
+	return format
 }
--- parse/input.go
+++ parse/input.go
@@ -1,8 +1,7 @@
-package parse
+package main
 
 import (
 	"bufio"
-	"context"
 	"fmt"
 	"log"
 	"os"
@@ -12,18 +11,28 @@ import (
 	rdb "github.com/CanobbioE/reelo/db"
 )
 
-const RANK_PATH = "./ranks"
+const RANK_PATH = "../ranks"
 
 var db *rdb.DB
 
+/*
 func init() {
 	db = rdb.NewDB()
+}
+*/
+
+type dataLine struct {
+	Name      string
+	Surname   string
+	City      string
+	Exercises int
+	Points    int
+	Time      int
 }
 
 // parseRankingFile reads a ranking from the correct file using the specified
 // format. The file's name must be in the format of "year_category.txt"
-func parseRankingFile(year int, category, format string) {
-	ctx := context.Background()
+func readRankingFile(year int, category string, format Format) {
 	filePath := fmt.Sprintf("%s/%d/%d_%s.txt", RANK_PATH, year, year, category)
 	file, err := os.Open(filePath)
 	if err != nil {
@@ -31,25 +40,62 @@ func parseRankingFile(year int, category, format string) {
 	}
 	defer file.Close()
 
-	// add the current year+category to the db
-	gID := db.Add(ctx, "giochi", year, category)
-
-	f := newFormat(strings.Split(strings.ToLower(format), " "))
-
-	// Here we do stuff and things with the input
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
-		data := strings.Split(scanner.Text(), " ")
-		updateDb(ctx, data, f, gID)
-		if err := scanner.Err(); err != nil {
-			log.Fatal(err)
-		}
+		// TODO: commenting this since we need to rewrite almost all of this.
+		//data := strings.Split(scanner.Text(), " ")
+		//updateDb(ctx, data, format, gID)
+		parseLine(format, scanner.Text())
+	}
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
 	}
 }
 
+func parseLine(format Format, input string) {
+	splitted := strings.Split(input, " ")
+	var result dataLine
+
+	fmt.Println(input)
+
+	// Handling base case.
+	// Otherwise it means there are fields with two or more words.
+	if len(splitted) == len(format) {
+		for fName, index := range format {
+			var err error
+			value := strings.Title(strings.ToLower(splitted[index]))
+			switch fName {
+			case "cognome":
+				result.Surname = value
+			case "nome":
+				result.Name = value
+			case "esercizi":
+				result.Exercises, err = strconv.Atoi(value)
+			case "punti":
+				result.Points, err = strconv.Atoi(value)
+			case "tempo":
+				result.Time, err = strconv.Atoi(value)
+			case "città":
+				result.City = strings.Title(value)
+			case "città(provincia)":
+				// TODO delete provincia from the input
+				result.City = strings.Title(value)
+			}
+			if err != nil {
+				log.Fatal("Could not convert data.", err)
+			}
+		}
+		fmt.Println(result)
+	} else {
+		log.Print("Not implemented")
+	}
+	fmt.Println()
+}
+
+/*
 // updateDb updates the database by adding all the data read from a single row
 // of the Ranking's file.
-func updateDb(ctx context.Context, data []string, f *Format, gID int) {
+func updateDb(ctx context.Context, data []string, f Format, gID int) {
 	var pID int
 	// Add the player information only if he doesn't already exist in the db
 	if !db.ContainsPlayer(ctx, data[f.Name], data[f.Surname]) {
@@ -66,3 +112,4 @@ func updateDb(ctx context.Context, data []string, f *Format, gID int) {
 	rID := db.Add(ctx, "risultato", time, data[f.Exercises], data[f.Score])
 	db.Add(ctx, "partecipazione", pID, gID, rID)
 }
+*/
