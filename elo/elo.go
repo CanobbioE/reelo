4f2d5f91212f4ffdf35f5d02880053cf5a0f89fb
CanobbioE <canobbio.e@gmail.com>
2019-01-20 19:40:50 +0100

    Added comments

--- elo/elo.go
+++ elo/elo.go
@@ -18,9 +18,12 @@ const NP_PENALTY = 0.9
 
 // Reelo returns the points for a given user calculated with a custom algorithm.
 // TODO: Reelo should not make DB calls
+// TODO: Handle Internationa Finals
+// TODO: from the DB recover: lastKnownYear, lastKnownCategory
 func Reelo(name, surname string) (reelo float64) {
 	var count, lastKnownYear int
 	var lastKnownCategory string
+	var intenationalFinal bool
 
 	// TODO: fix the for loop's range
 	for _, year := range []int{2002, 2018} {
@@ -36,12 +39,15 @@ func Reelo(name, surname string) (reelo float64) {
 
 		baseScore := float64(d + e*K_EXERCISES)
 
-		// if FINALE INTERNAZIONALE {
-		//    baseScore = baseScore * P_FINAL
-		// }
+		if intenationalFinal {
+			baseScore = baseScore * P_FINAL
+		}
 
+		// Categories homogenization:
+		// For each exercises a player is not supposed to solve we calculate
+		// her/his the probabilty of solving it.
 		for i := 1; i <= t-1; i++ {
-			errorFactor := float64(1-d) + float64(e*K_EXERCISES)/float64((K_EXERCISES*eMax+dMax))
+			errorFactor := 1 - float64(d+e*K_EXERCISES)/float64((K_EXERCISES*eMax+dMax))
 			difficultyFactor := float64(i) / float64(n+1)
 			nonResolutionProbability := 1 - errorFactor*difficultyFactor
 			baseScore += float64(K_EXERCISES+i) * nonResolutionProbability
@@ -51,20 +57,25 @@ func Reelo(name, surname string) (reelo float64) {
 		baseScore = baseScore * MULTIPLICATIVE_FACTOR
 
 		// TODO: actually check for category promotion
+		// Category promotion:
+		// If this year the player's category is inferior to the category she/he played
+		// most recently then we convert this year score to the most recent category
 		if lastKnownCategory > category {
 			oldAvg := db.GetAvgScore(year, category)
 			newAvg := db.GetAvgScore(year, lastKnownCategory)
 			newMax := db.GetMaxScore(year, lastKnownCategory)
-			scoreOfYear := baseScore
+			thisYearScore := baseScore
 
-			convertedScore := scoreOfYear * newAvg / oldAvg
+			convertedScore := thisYearScore * newAvg / oldAvg
+			// Do not exceed the maximum obtainable score
 			if convertedScore > newMax {
 				convertedScore = newMax
 			}
-
 			baseScore = convertedScore
 		}
 
+		// Aging:
+		// Most recent scores should weight more than past year's ones
 		agingFactor := 1 - 5/72*math.Log2(float64(lastKnownYear-year+1))
 		baseScore = baseScore * agingFactor
 
@@ -74,10 +85,17 @@ func Reelo(name, surname string) (reelo float64) {
 
 	reelo = reelo / float64(count)
 
+	// Anti-Exploit:
+	// To avoid the exploiting of a single partecipation, if the player has only
+	// one result and it's in the most recent year: her/his REELO decays
+	// TODO: do not use time.Now().Year(), recover a list of all known years
 	if count == 1 && lastKnownYear == time.Now().Year() {
 		reelo = reelo * ANTI_EXPLOIT
 	}
 
+	// No-partecipation penalty:
+	// If the player hasn't partecipated in the most recent year, his REELO decaysa
+	// TODO actually check if there's no result
 	// if NO RESULT IN THE LAST YEAR {
 	//    reelo = reelo * NP_PENALTY
 	// }
