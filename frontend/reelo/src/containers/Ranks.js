d8917fdf4024410be41d588136465daea93c9317
Edoardo Canobbio <canobbio.e@gmail.com>
2019-03-29 13:59:19 +0100

    Implemented ranks page (still needs back end)

--- frontend/reelo/src/containers/Ranks.js
+++ frontend/reelo/src/containers/Ranks.js
@@ -1,3 +1,46 @@
+import {
+	Grid,
+	Typography,
+	Table,
+	TableRow,
+	TableBody,
+	TableCell,
+	TableHead,
+} from '@material-ui/core';
+import React from 'react';
+
 export default function Ranks(props) {
-	return 'Ranks';
+	const rows = [
+		{id: 0, name: 'name', surname: 'surname', reelo: '123'},
+		{id: 1, name: 'name', surname: 'surname', reelo: '123'},
+		{id: 2, name: 'name', surname: 'surname', reelo: '123'},
+	];
+
+	const labels = ['Nome', 'Cognome', 'Reelo'];
+
+	const renderHeader = () =>
+		labels.map(label => <TableCell> {label} </TableCell>);
+
+	const renderRows = () =>
+		rows.map(row => (
+			<TableRow key={row.id}>
+				<TableCell>{row.name}</TableCell>
+				<TableCell>{row.surname}</TableCell>
+				<TableCell>{row.reelo}</TableCell>
+			</TableRow>
+		));
+
+	return (
+		<Grid container justify="center">
+			<Grid item xs={10}>
+				<Typography variant="h4">Classifiche</Typography>
+				<Table>
+					<TableHead>
+						<TableRow>{renderHeader()}</TableRow>
+					</TableHead>
+					<TableBody>{renderRows()}</TableBody>
+				</Table>
+			</Grid>
+		</Grid>
+	);
 }
