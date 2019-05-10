9233a407df4edca46cd37cba10c96a95f4b76431
Edoardo Canobbio <canobbio.e@gmail.com>
2019-05-10 04:11:04 +0200

    Added utilities for deployment and db initialization

---
+++ deploy/init.sql
@@ -0,0 +1,50 @@
+-- BACKUP DATABASE reelo
+--  TO DISK = ''
+--  WITH DIFFERENTIAL
+
+GRANT ALL PRIVILEGES ON reelo.* TO 'reeloUser'@'localhost' IDENTIFIED BY 'password';
+
+
+CREATE TABLE Giocatore (
+	id int AUTO_INCREMENT,
+	nome varchar(255) NOT NULL,
+	cognome varchar(255) NOT NULL,
+	reelo int,
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE Giochi (
+	id int AUTO_INCREMENT,
+	anno int,
+	categoria varchar(4),
+	PRIMARY KEY (id, anno, categoria)
+);
+
+CREATE TABLE Risultato (
+	id int AUTO_INCREMENT,
+	tempo int,
+	esercizi int,
+	punteggio int,
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE Partecipazione (
+	giocatore int,
+	giochi int,
+	risultato int,
+	sede varchar(255),
+	PRIMARY KEY (giocatore, giochi),
+	FOREIGN KEY (giocatore) REFERENCES Giocatore(id) ON UPDATE CASCADE,
+	FOREIGN KEY (giochi) REFERENCES Giochi(id) ON UPDATE CASCADE,
+	FOREIGN KEY (risultato) REFERENCES Risultato(id) ON DELETE SET NULL
+);
+
+CREATE TABLE Utenti (
+	nomeutente varchar(255),
+	parolachiave varchar(255),
+	PRIMARY KEY (nomeutente)
+);
+
+INSERT INTO Utenti (nomeutente, parolachiave)
+VALUES ('admin@canna.it', '0f07b601d88c88ec79eeac16bd05cbbd014b99ae45df1fcb0f80a221418e81c0');
+
