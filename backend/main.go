c513edcf9767a265c9aa6c361899bd2f8baf2edf
Edoardo Canobbio <canobbio.e@gmail.com>
2019-05-16 20:07:27 +0200

    Minor fixes

--- backend/main.go
+++ backend/main.go
@@ -5,8 +5,10 @@ package main
  * How to handle paris (do I exclude results from paris on every query?
  */
 import (
+	"io"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/CanobbioE/reelo/backend/controllers"
 	"github.com/CanobbioE/reelo/backend/utils/parse"
@@ -24,10 +26,22 @@ func init() {
 }
 
 func main() {
+	// Configure Logging
+	logFilePath := os.Getenv("LOG_FILE_PATH")
+	if logFilePath != "" {
+		f, err := os.OpenFile("./"+logFilePath, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+		if err != nil {
+			log.Fatalf("error opening file: %v", err)
+		}
+		defer f.Close()
+		wrt := io.MultiWriter(os.Stdout, f)
+		log.SetOutput(wrt)
+	}
+
 	router := mux.NewRouter()
 
 	router.HandleFunc("/ranks", controllers.GetRanks).Methods("GET")
-	router.HandleFunc("/admin", controllers.Login).Methods("POST", "OPTIONS")
+	router.HandleFunc("/admin", controllers.Login).Methods("POST")
 	router.HandleFunc("/upload", controllers.Upload).Methods("POST")
 
 	log.Fatal(http.ListenAndServe(":8080", handlers.CORS(
