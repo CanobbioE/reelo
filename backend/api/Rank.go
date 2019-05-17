d27b355611799af2842c17c7f009dc2a21e2941d
Edoardo Canobbio <canobbio.e@gmail.com>
2019-05-17 23:34:39 +0200

    Implemented Auth middleware, GET ranks and PUT forceReelo endpoints

---
+++ backend/api/Rank.go
@@ -0,0 +1,9 @@
+package api
+
+// Rank represent one entry of a ranking
+type Rank struct {
+	Name     string  `json:"name"`
+	Surname  string  `json:"surname"`
+	Category string  `json:"category"`
+	Reelo    float64 `json:"reelo"`
+}
