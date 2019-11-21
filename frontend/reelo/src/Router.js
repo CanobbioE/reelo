c960ed1ee3b823f32fbc538e335b7e8ee3654a97
Edoardo Canobbio <canobbio.e@gmail.com>
2019-11-21 21:31:36 +0100

    Added dnd provider (will be removed)

--- frontend/reelo/src/Router.js
+++ frontend/reelo/src/Router.js
@@ -3,6 +3,8 @@ import {BrowserRouter, Route} from 'react-router-dom';
 import {createStore, applyMiddleware} from 'redux';
 import React, {Component} from 'react';
 import {Provider} from 'react-redux';
+import {DndProvider} from 'react-dnd';
+import HTML5Backend from 'react-dnd-html5-backend';
 import reduxThunk from 'redux-thunk';
 
 import NavBar from './components/NavBar';
@@ -14,6 +16,7 @@ import Ranks from './containers/Ranks';
 import Admin from './containers/Admin';
 import Upload from './containers/Upload';
 import EditAlgorithm from './containers/EditAlgorithm';
+import Namesakes from './containers/Namesakes';
 
 import './index.css';
 
@@ -28,24 +31,23 @@ const store = createStore(
 class Router extends Component {
 	render() {
 		return (
-			<MuiThemeProvider theme={properties.theme}>
-				<Provider className="bg-white" store={store}>
-					<BrowserRouter>
-						<NavBar />
-						<div>
-							<Route exact path={Globals.routes.home} component={Ranks} />
-							<Route exact path={Globals.routes.about} component={About} />
-							<Route exact path={Globals.routes.upload} component={Upload} />
-							<Route exact path={Globals.routes.admin} component={Admin} />
-							<Route
-								exact
-								path={Globals.routes.varchange}
-								component={EditAlgorithm}
-							/>
-						</div>
-					</BrowserRouter>
-				</Provider>
-			</MuiThemeProvider>
+			<DndProvider backend={HTML5Backend}>
+				<MuiThemeProvider theme={properties.theme}>
+					<Provider className="bg-white" store={store}>
+						<BrowserRouter>
+							<NavBar />
+							<div>
+								<Route exact path={Globals.routes.home} component={Ranks} />
+								<Route exact path={Globals.routes.about} component={About} />
+								<Route exact path={Globals.routes.upload} component={Upload} />
+								<Route exact path={Globals.routes.admin} component={Admin} />
+								<Route exact path={Globals.routes.namesakes} component={Namesakes} />
+								<Route exact path={Globals.routes.varchange} component={EditAlgorithm} />
+							</div>
+						</BrowserRouter>
+					</Provider>
+				</MuiThemeProvider>
+			</DndProvider>
 		);
 	}
 }
