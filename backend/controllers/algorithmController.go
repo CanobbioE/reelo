7c071898ba9972039cb5718d369a7fe6f75be14b
Edoardo Canobbio <canobbio.e@gmail.com>
2020-01-01 19:16:58 +0100

    Added return statements

--- backend/controllers/algorithmController.go
+++ backend/controllers/algorithmController.go
@@ -29,16 +29,19 @@ func UpdateAlgorithm(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		log.Printf("Error while  reading costants body: %v", err)
 		http.Error(w, "can't update costants", http.StatusBadRequest)
+		return
 	}
 	err = json.Unmarshal(body, &c)
 	if err != nil {
 		log.Printf("Error while unmarshalling costants: %v", err)
 		http.Error(w, "can't update costants", http.StatusBadRequest)
+		return
 	}
 	err = services.UpdateAlgorithm(context.Background(), c)
 	if err != nil {
 		log.Printf("Error updating costants: %v", err)
 		http.Error(w, "can't update costants", http.StatusInternalServerError)
+		return
 	}
 	log.Println("Costants updated")
 	return
